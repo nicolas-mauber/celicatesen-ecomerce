eb979626655ea393b66f4ce1aafcd07fa49e1436
Nicolas <nicolasensexto@gmail.com>
2022-12-06 22:34:03 -0300

    navbar terminada

--- src/Componentes/NavBar.jsx
+++ src/Componentes/NavBar.jsx
@@ -34,79 +34,84 @@ export default NavBar;
 
 const NavContainer = styled.nav`
   h2 {
-    color: #fff;
+    color: white;
     font-weight: 400;
     span {
       font-weight: bold;
     }
   }
-
   padding: 0.4rem;
   background-color: #333;
   display: flex;
   align-items: center;
   justify-content: space-between;
-
-  a{
-    color: "#fff",
-    text-decoration: "none",
-    margin-right: "1rem",
-    }
-  
-  .links{
+  a {
+    color: white;
+    text-decoration: none;
+    margin-right: 1rem;
+  }
+  .links {
     position: absolute;
-    top: 50%;
-    left: 50%;
+    top: -700px;
+    left: -2000px;
+    right: 0;
     margin-left: auto;
     margin-right: auto;
     text-align: center;
-    a{
-        color: #333;
-        font-size: 1.5rem;
-        display: block;
+    transition: all 0.5s ease;
+    a {
+      color: white;
+      font-size: 2rem;
+      display: block;
     }
     @media (min-width: 768px) {
-        position: inital;
-        margin:0;
-        a{
-            font-size: 1rem;
-            color: #black;
-            display: inline;
-        }
+      position: initial;
+      margin: 0;
+      a {
+        font-size: 1rem;
+        color: white;
+        display: inline;
+      }
+      display: block;
     }
   }
-
   .links.active {
     width: 100%;
     display: block;
     position: absolute;
     margin-left: auto;
     margin-right: auto;
-    top: 100px;
-    left: 100px; 
-    right: 100px;
+    top: 30%;
+    left: 0;
+    right: 0;
     text-align: center;
-    a{
-     color: #black;
+    a {
+      font-size: 2rem;
+      margin-top: 1rem;
+      color: white;
     }
-  } 
-
+  }
   .burguer {
     @media (min-width: 768px) {
       display: none;
     }
   }
-
 `;
-
 const BgDiv = styled.div`
+  background-color: #222;
   position: absolute;
-  background-color: #444;
-  top: 0;
-  left: 0;
-  &active {
+  top: -1000px;
+  left: -1000px;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  transition: all 0.6s ease;
+
+  &.active {
+    border-radius: 0 0 80% 0;
+    top: 0;
+    left: 0;
     width: 100%;
     height: 100%;
   }
-  z-index: -1;
 `;
